d994ff6d9f7ed678ba41a0883f9b38e6289e0663
lmyaoi <renzo.russel@gmail.com>
2017-12-09 21:05:30 +0100

    made it interactable so you can change currency while running

--- index.js
+++ index.js
@@ -12,20 +12,44 @@ var options = {
 };
 
 var cap = new CoinMarketCap(options);
+var stdin = process.openStdin();
+
+function getPrice(info) {
+  var ret;
+  switch (currency) {
+    case "EUR":
+      ret = info.price_eur;
+      break;
+    case "USD":
+      ret = info.price_usd ;
+      break;
+    default:
+      return "The currency \"" + currency + "\" is not currently supported.";
+  }
+  return ret + " " + currency;
+}
+
+function setCurrency(input) {
+  input = input.toString().trim();
+  switch (input) {
+    case "EUR":
+      currency = input;
+      break;
+    case "USD":
+      currency = input;
+      break;
+    default:
+      console.log("> The currency \"" + input + "\" is not currently supported.");
+      return;
+  }
+  console.log("> The current currency is: " + input);
+}
+
 
 cap.on(coin, (info) => {
   console.log("> " + getPrice(info));
 });
 
-function getPrice(info) {
-  switch (currency) {
-    case "EUR":
-      return info.price_eur + " " + currency;
-      break;
-    case "USD":
-      return info.price_usd;
-      break;
-    default:
-      return "The currency \"" + currency + "\" is not currently supported.";
-  }
-}
+stdin.addListener("data", (input) => {
+  setCurrency(input)
+});
